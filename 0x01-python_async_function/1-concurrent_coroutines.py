8e82adbaab93a4e06b84ec5cdc8490f317a99503
procode3 <wr.t8r1@gmail.com>
2023-07-10 04:23:46 -0700

    fixing sorting

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,9 +8,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """concurrent execution"""
     delays = []
-    returns = []
     for i in range(n):
         delays.append(await wait_random(max_delay))
-    for i in delays:
-        returns.append(sorted(i))
-    return returns
+    return sorted(delays)
