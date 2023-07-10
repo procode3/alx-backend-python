3c96e85486f90b017b8f16ee8df889ec78b966b9
procode3 <wr.t8r1@gmail.com>
2023-07-10 05:16:52 -0700

    Function that measures time for async func to complete writing

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!usr/bin/env python3
+"""finding time delta for the async functions"""
+
+from time import time
+import asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+def measure_time(n: int, max_delay:int) ->float:
+    """Measuring the time difference between times"""
+    start = time()
+    asyncio.run(wait_n(n, max_delay))
+    return(time() - start)	
+    
+    
+
