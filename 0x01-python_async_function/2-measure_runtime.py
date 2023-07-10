f5ef371d078bb907853e7960154b78bd7f66997f
procode3 <wr.t8r1@gmail.com>
2023-07-10 05:27:07 -0700

    Fixing return value

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -5,12 +5,12 @@ from time import time
 import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
-def measure_time(n: int, max_delay:int) ->float:
+
+def measure_time(n: int, max_delay: int) -> float:
     """Measuring the time difference between times"""
     start = time()
+
     asyncio.run(wait_n(n, max_delay))
+
     total_time = time() - start
     return total_time / n
-    
-    
-
