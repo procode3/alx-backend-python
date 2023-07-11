7e6bf33fb6aef150b30e7c3e0f378a8345394737
procode3 <wr.t8r1@gmail.com>
2023-07-11 03:39:02 -0700

    Updated annotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,9 @@
 """Generators using yield"""
 import asyncio
 import random
-from tryping import List
+from typing import List
 
-async def async_generator() -> float:
+async def async_generator() -> List[float]:
     """Coroutine will loop 10 times, each time asynchronously wait 1 second,"""
     for i in range(10):
         await asyncio.sleep(1)
