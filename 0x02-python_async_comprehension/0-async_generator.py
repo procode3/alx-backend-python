8e4967512617894b16ef4a94d5e9bc6ea83c7c99
procode3 <wr.t8r1@gmail.com>
2023-07-11 03:39:57 -0700

    Updated pycode style

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,6 +4,7 @@ import asyncio
 import random
 from typing import List
 
+
 async def async_generator() -> List[float]:
     """Coroutine will loop 10 times, each time asynchronously wait 1 second,"""
     for i in range(10):
