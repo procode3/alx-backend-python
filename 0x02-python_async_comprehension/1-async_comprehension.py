982cc1691f93c0df8143658770b2fc6c584365ef
procode3 <wr.t8r1@gmail.com>
2023-07-11 03:35:20 -0700

    Updated pycode styles

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Coroutine will collect 10 random numbers using an async comprehensing over async_generator"""
+
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Implementing async comprehensing"""
+    return [number async for number in async_generator()]
---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Couroutine that will exercute async_comprehension asychronously"""
+
+
+from time import time
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Measuring time for for runs of async_comprehension"""
+    start = time()
+    await asyncio.gather(async_comprehension(), async_comprehension(), async_comprehension(), async_comprehension())
+    return time() - start
+ 
